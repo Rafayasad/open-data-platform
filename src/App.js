a55c243908553a8e5df5ceae3fc7f9fa03c389a4
Rafay Asad <rafay.asad@mmcgbl.com>
2023-01-31 20:29:18 +0500

    i18n configuration done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
+import { useTranslation, Trans } from 'react-i18next';
 
 const Home = lazy(() => import('./pages/Home'))
 
+
 function App() {
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    document.getElementsByTagName('html')[0].setAttribute("dir", i18n.language === "en" ? "ltr" : "rtl")
+  }, [i18n.language])
   return (
     <Home />
   );
---
+++ src/i18n/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Locale_en from '../i18n/Locales/en/translation.json';
+import Locale_ar from '../i18n/Locales/ar/translation.json';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+        resources: {
+            en: {
+                translation: Locale_en
+            },
+            ar: {
+                translation: Locale_ar
+            }
+        }
+    });
+
+export default i18n;
