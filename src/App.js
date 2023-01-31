70a39e415a20b7156de07b6db772d7a9cbd2bcd2
Alishan <alishan@mmcgbl.com>
2023-01-31 12:54:04 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Heading from './components/Heading';
 import { lazy } from 'react';
 
 const Home = lazy(() => import('./pages/Home'))
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <div className='row align-items-center justify-content-center'>
-    //       <div className='col-xxl-8 col-lg-8 col-md-12 col-sm-12 col-xsm-12'>
-    //         <Heading heading={"Making data available for everyone"} />
-    //       </div>
-    //     </div>
-    //   </header>
-    // </div>
     <Home />
   );
 }
