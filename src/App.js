720461d00470854bb355f48c4b5d28888c917bf4
Alishan <alishan@mmcgbl.com>
2023-02-01 23:14:50 +0500

    Update Application Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { lazy } from 'react';
-import Applications from './pages/Applications';
 
 const Home = lazy(() => import('./pages/Home'))
 const Dataset = lazy(() => import('./pages/Dataset'))
 const About = lazy(() => import('./pages/About'))
+const Applications = lazy(() => import('./pages/Applications'))
 
 function App() {
   return (
