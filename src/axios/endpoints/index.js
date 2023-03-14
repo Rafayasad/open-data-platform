d324be893400c7f7aaf058c2f92f5600499528c8
Alishan <alishan@mmcgbl.com>
2023-03-14 11:37:06 +0500

    Enviornment Setup

--- src/axios/endpoints/index.js
+++ src/axios/endpoints/index.js
@@ -52,42 +52,81 @@ export const endpoints = {
         return client.get(`/jsonapi/node/stories?filter[id]=${id}`)
     },
     getCRSFToken: () => {
-        return axios.get('https://data.abudhabi/session/token')
+        return client.get('/session/token')
     },
+    // getCRSFToken: () => {
+    //     return axios.get('https://data.abudhabi/session/token')
+    // },
     login: (data, headers) => {
-        return axios.post('http://10.241.40.69:30418/user/login?_format=json', data, { headers })
+        return client.post('/user/login?_format=json', data, { headers })
     },
+    // login: (data, headers) => {
+    //     return axios.post('http://10.241.40.69:30418/user/login?_format=json', data, { headers })
+    // },
     register: (data, headers) => {
-        return axios.post('http://10.241.40.69:30418/apis/register.php', data, { headers })
+        return client.post('/apis/register.php', data, { headers })
     },
+    // register: (data, headers) => {
+    //     return axios.post('http://10.241.40.69:30418/apis/register.php', data, { headers })
+    // },
     getInsightsReport: (data, options) => {
-        return axios.post('http://192.168.0.23:82/apis/reports/insight_report.php', data, options)
+        return client.post('/apis/reports/insight_report.php', data, options)
     },
+    // getInsightsReport: (data, options) => {
+    //     return axios.post('http://192.168.0.23:82/apis/reports/insight_report.php', data, options)
+    // },
     getPublishersReport: (data, options) => {
-        return axios.post('http://192.168.0.23:82/apis/reports/publishers_report.php', data, options)
+        return client.post('/apis/reports/publishers_report.php', data, options)
     },
+    // getPublishersReport: (data, options) => {
+    //     return axios.post('http://192.168.0.23:82/apis/reports/publishers_report.php', data, options)
+    // },
     getDatasetsReport: (data, options) => {
-        return axios.post('http://192.168.0.23:82/apis/reports/dataset_report.php', data, options)
+        return client.post('/apis/reports/dataset_report.php', data, options)
     },
+    // getDatasetsReport: (data, options) => {
+    //     return axios.post('http://192.168.0.23:82/apis/reports/dataset_report.php', data, options)
+    // },
     getSearch: () => {
-        return axios.get('https://data.abudhabi/apis/search.php')
+        return client.get('/apis/search.php')
     },
+    // getSearch: () => {
+    //     return axios.get('https://data.abudhabi/apis/search.php')
+    // },
     postSearch: (data) => {
-        return axios.post('https://data.abudhabi/apis/search.php', data)
+        return client.post('/apis/search.php', data)
     },
+    // postSearch: (data) => {
+    //     return axios.post('https://data.abudhabi/apis/search.php', data)
+    // },
     recoverPassword: (data, headers) => {
-        return axios.post('http://10.241.40.69:30418/apis/reset_email.php', data, { headers })
+        return client.post('/apis/reset_email.php', data, { headers })
     },
+    // recoverPassword: (data, headers) => {
+    //     return axios.post('http://10.241.40.69:30418/apis/reset_email.php', data, { headers })
+    // },
     resetPassword: (data, headers) => {
-        return axios.post('http://10.241.40.69:30418/xwordxssapx.php', data, { headers })
+        return client.post('/xwordxssapx.php', data, { headers })
     },
+    // resetPassword: (data, headers) => {
+    //     return axios.post('http://10.241.40.69:30418/xwordxssapx.php', data, { headers })
+    // },
     viewCount: (data) => {
-        return axios.post('https://data.abudhabi/apis/view_count.php', data);
+        return client.post('/apis/view_count.php', data);
     },
+    // viewCount: (data) => {
+    //     return axios.post('https://data.abudhabi/apis/view_count.php', data);
+    // },
     realTimeApis: (data) => {
-        return axios.get('https://data.abudhabi/jsonapi/node/real_time_apis', data);
+        return client.get('/jsonapi/node/real_time_apis', data);
     },
+    // realTimeApis: (data) => {
+    //     return axios.get('https://data.abudhabi/jsonapi/node/real_time_apis', data);
+    // },
     getPrivacyPolicy: () => {
-        return axios.get("https://data.abudhabi/jsonapi/node/privacy_policy");
+        return client.get("/jsonapi/node/privacy_policy");
     }
+    // getPrivacyPolicy: () => {
+    //     return axios.get("https://data.abudhabi/jsonapi/node/privacy_policy");
+    // }
 } 
