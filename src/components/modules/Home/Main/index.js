b19364df7a11b30cefbbb522fd5295bb73e4efae
Rafay Asad <rafay.asad@mmcgbl.com>
2023-04-07 14:45:38 +0500

    fixes updated

--- src/components/modules/Home/Main/index.js
+++ src/components/modules/Home/Main/index.js
@@ -8,10 +8,12 @@ import backgroundMobile from '../../../../assets/images/BG-MOB.png';
 import { useTranslation } from "react-i18next";
 import i18n from "../../../../i18n/i18n";
 import { locales } from "../../../../i18n/helper";
+import { useSelector } from "react-redux";
 
 const Main = memo((props) => {
 
-    const { onSearch, onClickExplore, onApplyFilter, filterData } = props
+    const { onSearch, onClickExplore, onApplyFilter, filterData } = props;
+    const { datasetsSuggestion } = useSelector((state) => state.facets);
 
     const { t } = useTranslation()
 
@@ -38,7 +40,14 @@ const Main = memo((props) => {
                         <Row>
                             <Col />
                             <Col xs={12} md={10} lg={8} className="py-3">
-                                <Search filterData={filterData} nofocuseffect onClickApplyFilter={onApplyFilter} placeholder={t("searchPlaceholder")} filter onPressEnter={onSearch} />
+                                <Search
+                                    filterData={filterData}
+                                    // nofocuseffect
+                                    searchData={i18n.language === locales.AR ? datasetsSuggestion?.ar : datasetsSuggestion?.en}
+                                    onClickApplyFilter={onApplyFilter}
+                                    placeholder={t("searchPlaceholder")}
+                                    filter
+                                    onPressEnter={onSearch} />
                             </Col>
                             <Col />
                         </Row>
