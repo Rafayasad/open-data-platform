f902003ce0328399ab7e520eedc93438a4209833
Alishan <alishan@mmcgbl.com>
2023-05-04 15:53:04 +0500

    Publisher page added

--- src/pages/Publisher/index.js
+++ src/pages/Publisher/index.js
@@ -11,57 +11,56 @@ import LowerFooter from '../../components/modules/Footer/LowerFooter';
 import { useSelector } from "react-redux";
 import View from "../../components/modules/View";
 
-const Applications = memo(() => {
+const Publisher = memo(() => {
 
     const { t } = useTranslation()
-    const applications = useSelector(state => state.application.applications)
-    const cardsDiv = document.getElementById("cards");
+    // const applications = useSelector(state => state.application.applications)
+    // const cardsDiv = document.getElementById("cards");
 
-    const [displayApplications, setDisplayApplications] = useState();
+    // const [displayApplications, setDisplayApplications] = useState();
 
+    // const [totalCount, setTotalCount] = useState(0);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [rowsPerPage, setRowsPerPage] = useState(9);
 
-    const [totalCount, setTotalCount] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [rowsPerPage, setRowsPerPage] = useState(9);
+    // useEffect(() => {
+    //     if (applications) {
+    //         let arr = [...applications]
+    //         let x = arr.slice(0, rowsPerPage)
+    //         setDisplayApplications(x)
+    //     }
+    // }, [applications])
 
-    useEffect(() => {
-        if (applications) {
-            let arr = [...applications]
-            let x = arr.slice(0, rowsPerPage)
-            setDisplayApplications(x)
-        }
-    }, [applications])
+    // const onChangePage = useCallback((page) => {
 
-    const onChangePage = useCallback((page) => {
+    //     if (page) {
+    //         let start = (page - 1) * rowsPerPage
+    //         let end = (start + rowsPerPage)
 
-        if (page) {
-            let start = (page - 1) * rowsPerPage
-            let end = (start + rowsPerPage)
+    //         let arr = [...applications]
+    //         let x = arr.slice(start, end);
+    //         setDisplayApplications(x);
+    //         setCurrentPage(page)
+    //     }
 
-            let arr = [...applications]
-            let x = arr.slice(start, end);
-            setDisplayApplications(x);
-            setCurrentPage(page)
-        }
+    // }, [currentPage, displayApplications])
 
-    }, [currentPage, displayApplications])
+    // const onClickCard = useCallback((id) => {
 
-    const onClickCard = useCallback((id) => {
+    //     if (id) {
 
-        if (id) {
+    //         let app = applications.find(item => item.id === id)
 
-            let app = applications.find(item => item.id === id)
+    //         window.open(app.applicationURL, '_blank')
 
-            window.open(app.applicationURL, '_blank')
+    //     }
 
-        }
-
-    })
+    // })
 
     return (
         // <div style={{ maxWidth: "1800px", margin: "auto" }}>
             <View theme="dark" footerTitle={t("GetMore")} footerButton={t("registerNow")}>
-                <div className="my-5 pt-5 max-width">
+                <div className="my-5 pt-5">
                     <Main />
                     <div className="my-5" id="cards">
                         <Cards type="image-outer-text" data={displayApplications} onClick={onClickCard} />
@@ -80,4 +79,4 @@ const Applications = memo(() => {
     )
 })
 
-export default Applications;
+export default Publisher;
