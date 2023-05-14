a2faf7da81c6a0b7dd7ab7400fd366f72b217b8c
Rafay Asad <rafay.asad@mmcgbl.com>
2023-05-15 00:31:14 +0500

    update env

--- src/components/elements/Recaptcha/index.js
+++ src/components/elements/Recaptcha/index.js
@@ -1,18 +1,22 @@
-import { memo } from "react";
+import { memo, useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const ReactRecaptcha = memo((props) => {
 
     const { callBack } = props;
 
+    const captchaRef = useRef(null)
+
     function onChange(value) {
+        console.log("RECAPTCHA", value);
         callBack(value);
     }
 
     return (
         <ReCAPTCHA
-            sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            sitekey={"6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"}
             onChange={onChange}
+            ref={captchaRef}
             type="image"
             size="compact"
         // badge="inline"
